0ad91a423e71a263a25a412b5ab7d099fd9dc940
zakariadachi <dachiziko@gmail.com>
2026-02-06 12:47:50 +0100

    feat: add heading to index page
    * Added <h1> element to display a greeting
    * Enhanced page layout for better user experience

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>hi hi</h1>
     <p>hi</p>
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis enim possimus, neque natus repellendus obcaecati minus facere culpa amet, commodi atque iste! Hic, corrupti? Sunt, fuga aut. Numquam, praesentium iste!
 </body>
